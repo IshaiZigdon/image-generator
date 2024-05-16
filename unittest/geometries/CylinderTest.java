6d90b4c58b98ce73744ae965dab28b66d66ff352
ishai <ishaiz102@gmail.com>
2024-05-16 13:04:10 +0300

    add empty test files for everything but Double3 and Util

---
+++ unittest/geometries/CylinderTest.java
@@ -0,0 +1,23 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * unit test for Geometries.Cylinder
+ */
+class CylinderTest {
+    /**
+     * Test method for {@link geometries.Cylinder#getNormal(primitives.Point)}
+     */
+
+    @Test
+    void testGetNormal() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
+    }
+}

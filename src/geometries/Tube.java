a0a30b16e37cc2a0e72c9f6bea36c1d7b1cb9fae
ishai <ishaiz102@gmail.com>
2024-06-07 12:53:48 +0300

    Tube is WORKING!!!

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -4,8 +4,8 @@ import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
 
-import java.util.ArrayList;
 import static primitives.Util.*;
+
 import java.util.List;
 
 import static java.lang.Math.sqrt;
@@ -45,49 +45,60 @@ public class Tube extends RadialGeometry {
     @Override
     public List<Point> findIntersections(Ray ray) {
         //Axis direction ==d
-        Vector d =axis.getDirection();
+        Vector d = axis.getDirection();
         //ray direction ==v
         Vector v = ray.getDirection();
         // w= distance between the point of ray and the point of axis
-        double a,b,c;
+        double a, b, c;
         //dv = d x v
         Vector dv;
         // w= distance between the point of ray and the point of axis
         Vector w;
         //dw = d x w
         Vector dw;
-        try{
-            dv = d.crossProduct(v);
-            w =ray.getHead().subtract(axis.getHead());
-            dw = d.crossProduct(w);
-        }
-        catch(IllegalArgumentException msg){
+        if (d.dotProduct(v) == d.length() * v.length())
             return null;
+        dv = d.crossProduct(v);
+        if (ray.getHead().equals(axis.getHead())) {
+            a = alignZero((dv.lengthSquared()) / d.lengthSquared());
+            return List.of(ray.getPoint(alignZero(radius / Math.sqrt(a))));
         }
+        //but for now it will be null
+        w = ray.getHead().subtract(axis.getHead());
+        if (d.dotProduct(w) == d.length() * w.length()) {
+            double temp = v.dotProduct(d) / d.dotProduct(d);
+            if (temp != 0) {
+                Vector tempD = d.scale(-temp);
+                Vector v_90 = v.add(tempD);
+                return List.of(ray.getPoint(alignZero(radius / v_90.length())));
+            }
+            return List.of(ray.getPoint(alignZero(radius / v.length())));
+        }
+        dw = d.crossProduct(w);
         //a= |d x v|^2/|d|^2
-        a = alignZero( (dv.lengthSquared())/d.lengthSquared());
+        a = alignZero((dv.lengthSquared()) / d.lengthSquared());
         //b = 2* (d x w) * (d x v)  / |d|^2
-        b =alignZero( (2* dv.dotProduct(dw)) / d.lengthSquared());
+        b = alignZero((2 * dv.dotProduct(dw)) / d.lengthSquared());
         //c = |d x w|^2 / |d|^2 - r^2
-        c =alignZero( (dw.lengthSquared()/ d.lengthSquared() )-radiusSquared);
+        c = alignZero((dw.lengthSquared() / d.lengthSquared()) - radiusSquared);
 
-        double discriminant = alignZero(b*b - 4*a*c);
-        if (discriminant < 0) {
-            return null;
-        }
-        double t1,t2;
-        t1 = (-b-Math.sqrt(discriminant) ) / 2*a;
-        t2 = (-b+Math.sqrt(discriminant) ) / 2*a;
-        if (discriminant == 0||t2 ==0||t1==0) {
-            //t1 is the point
-            if(t1 ==0)
+
+        double discriminant = alignZero(b * b - 4 * a * c);
+        if (discriminant >= 0) {
+            double t1, t2;
+            t1 = (-b - Math.sqrt(discriminant)) / 2 * a;
+            t2 = (-b + Math.sqrt(discriminant)) / 2 * a;
+            if (t2 <= 0)
+                return null;
+            if (t1 <= 0) {
                 return List.of(ray.getPoint(alignZero(t2)));
-            return List.of(ray.getPoint(alignZero(t1)));
+            } else if (discriminant == 0)
+                return null;
+            return List.of(ray.getPoint(alignZero(t1)), ray.getPoint(alignZero(t2)));
         }
-        //t1 and t2 are the points
-        //discriminant must be >0
-        return List.of(ray.getPoint(alignZero(t1)), ray.getPoint(alignZero(t2)));
-
-
+        return null;
     }
 }
+
+
+

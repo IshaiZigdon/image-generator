6edd2cbf4d15f93dd76fc6ec086553e55d0884d9
Zaki <yhz0533293313@gmail.com>
2024-06-11 11:04:20 +0300

    dan check fixes beggining of phase 5

--- unittest/renderer/ImageWriterTest.java
+++ unittest/renderer/ImageWriterTest.java
@@ -3,18 +3,20 @@ package renderer;
 import org.junit.jupiter.api.Test;
 import primitives.Color;
 
+import static primitives.Util.isZero;
+
 class ImageWriterTest {
 
 
     @Test
     void writeToImage() {
         int t =50;
-        int height = 10*t, /*800*/ width = 16*t;//500
+        int height = 10*t, /*500*/ width = 16*t;//800
         ImageWriter imageWriter = new ImageWriter("skadush", width, height);
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (i%t == 0 || j%t == 0)
+                if (isZero(i%t)|| isZero(j%t))
                     imageWriter.writePixel(j,i,new Color(0,0,128));
                 else
                     imageWriter.writePixel(j,i,new Color(255,215,0));
@@ -22,5 +24,4 @@ class ImageWriterTest {
         }
         imageWriter.writeToImage();
     }
-
 }
